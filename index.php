c29a4c4c35f593f8f7c8daa155fa472f031a8402
fl-ff <110607003+fl-ff@users.noreply.github.com>
2022-12-26 17:29:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,7 @@
 	<title>the fruit bowl</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="theme-color" content="#COLORHEX">
-	<meta property="og:site_name" content="Name of your website">
-	<meta property="og:url" content="https://domain.com">
-	<meta property="og:title" content="Name of your page">
-	<meta property="og:description" content="A little description of this page">
-	<meta property="og:type" content="website">
-	<meta name="og:image" itemprop="image" content="img/64x64.png">
+
 </head>
 <body>
 	<header>
@@ -27,23 +21,6 @@
 	</div>
 	<footer>
 		<div id="desc">
-		<?php
-		$x = rand(0,31);
-		switch($x){
-			case(0):
-				echo("kill");
-				break;
-			case(1):
-				echo("dont");
-				break;
-			case(2):
-				echo("brooooooo what the hellll is this");
-				break;
-			default:
-				echo("");
-				break;
-		}
-		?>
 		</div>
 		<a href=""><img src="img/64x64.png"></a>
 	</footer>
